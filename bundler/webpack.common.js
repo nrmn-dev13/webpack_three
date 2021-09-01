fbce3fd6bfe3372118d84251cf433293744bb4e1
normanvnfc <norman@venuerific.com>
2021-09-01 10:27:07 +0700

    Update webpack and styling

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -3,75 +3,90 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 
 module.exports = {
-    entry: path.resolve(__dirname, '../src/index.js'),
-    output:
-    {
-        filename: 'bundle.[hash].js',
-        path: path.resolve(__dirname, '../dist')
-    },
-    devtool: 'source-map',
-    plugins:
+  entry: path.resolve(__dirname, '../src/index.js'),
+  output:
+  {
+    filename: 'bundle.[hash].js',
+    path: path.resolve(__dirname, '../dist')
+  },
+  devtool: 'source-map',
+  plugins:
     [
-        new CopyWebpackPlugin([ { from: path.resolve(__dirname, '../static') } ]),
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, '../src/index.html'),
-            minify: true
-        })
+      new CopyWebpackPlugin([{ from: path.resolve(__dirname, '../static') }]),
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, '../src/index.html'),
+        minify: true
+      })
     ],
-    module:
-    {
-        rules:
-        [
-            // HTML
-            {
-                test: /\.(html)$/,
-                use: ['html-loader']
-            },
+  module:
+  {
+    rules:
+      [
+        // HTML
+        {
+          test: /\.(html)$/,
+          use: ['html-loader']
+        },
 
-            // JS
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use:
-                [
-                    'babel-loader'
-                ]
-            },
+        // JS
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use:
+            [
+              'babel-loader'
+            ]
+        },
 
-            // CSS
-            {
-                test: /\.css$/,
-                use:
-                [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
+        // CSS
+        {
+          test: /\.css$/,
+          use:
+            [
+              'style-loader',
+              'css-loader'
+            ]
+        },
 
-            // Images
-            {
-                test: /\.(jpg|png|gif|svg)$/,
-                use:
-                [
-                    {
-                        loader: 'file-loader',
-                        options:
-                        {
-                            outputPath: 'assets/images/'
-                        }
-                    }
-                ]
-            },
+        // Images
+        {
+          test: /\.(jpg|png|gif|svg)$/,
+          use:
+            [
+              {
+                loader: 'file-loader',
+                options:
+                {
+                  outputPath: 'assets/images/'
+                }
+              }
+            ]
+        },
+        // Multi page
+        {
+          test: /\.html$/,
+          use:
+            [
+              {
+                loader: 'file-loader',
+                options:
+                {
+                  name: '[name].[ext]'
+                }
+              }
+            ],
+            exclude: path.resolve(__dirname, '../src/index.html')
+        },
 
-            // Shaders
-            {
-                test: /\.(glsl|vs|fs|vert|frag)$/,
-                exclude: /node_modules/,
-                use: [
-                    'raw-loader',
-                    'glslify-loader'
-                ]
-            }
-        ]
-    }
+        // Shaders
+        {
+          test: /\.(glsl|vs|fs|vert|frag)$/,
+          exclude: /node_modules/,
+          use: [
+            'raw-loader',
+            'glslify-loader'
+          ]
+        }
+      ]
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './style/main.css'
+import '../src/inside.html'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
